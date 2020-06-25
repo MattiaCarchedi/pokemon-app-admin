6f2c438aeb17bd5d50aa51e6971b9b53ee35cdf0
Mattia Carchedi <mattia.carchedi@gmail.com>
2020-06-25 10:56:23 +0200

    implemented dummy form

--- src/App.js
+++ src/App.js
@@ -1,26 +1,51 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+
+import "./App.css";
+
+import CssBaseline from "@material-ui/core/CssBaseline";
+import TextField from "@material-ui/core/TextField";
+import Container from "@material-ui/core/Container";
+import Button from "@material-ui/core/Button";
+
+function FixedContainer() {
+	return (
+		<React.Fragment>
+			<CssBaseline />
+			<Container
+				style={{
+          display: 'flex',
+          justifyContent: 'center',
+          flexDirection: 'column',
+          alignItems: 'center',
+					padding: 50,
+					textAlign: "center",
+					backgroundColor: "#3dfff9",
+					position: "absolute",
+					height: "100vh",
+					minWidth: "100%",
+				}}
+				fixed
+			>
+				<TextField style={{ color: "black" }} label='User Name' />
+				<br />
+				<br />
+				<TextField id='standard-basic' label='Password' type='password' />
+				<br />
+				<br />
+				<Button style={{ margin: 50, borderRadius: 20 }} variant='contained'>
+					Login
+				</Button>
+			</Container>
+		</React.Fragment>
+	);
+}
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<div className='App'>
+			<FixedContainer />
+		</div>
+	);
 }
 
 export default App;
